4c20927122ece5137d9d2f1a4952351ee6c3d8b2
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-18 12:58:48 +0000

    added positive or negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is posistive\n", n);
+		printf("%d is posistive", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("%d is negative", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%d is zero", n);
 	else
 		return (0);
 	return (0);
