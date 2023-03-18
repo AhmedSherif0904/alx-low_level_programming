29155337f25225bac7799a0e9097fc97e6d1cc79
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-18 15:14:12 +0000

    added print numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: printing alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i<=9; i++)
+	{
+		putchar(i+48);
+	}
+	putchar('\n');
+	return (0);
+}
