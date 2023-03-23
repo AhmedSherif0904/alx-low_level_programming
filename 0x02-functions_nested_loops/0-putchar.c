c12bd340c0659a148e36915d510119d32ff3beaa
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-23 09:41:50 +0000

    added putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,18 @@
  *
  * description : prints putchar
  *
- * return: 0
+ * Return: 0
  */
 
-int main(void) {
-  char str[] = "_putchar";
-  int ch;
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
 
-  for (ch = 0; ch < 8; ch++)
-    _putchar(str[ch]);
-  _putchar('\n');
-
-  return (0);
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(str[ch]);
+		_putchar('\n');
+	}
+	return (0);
 }
