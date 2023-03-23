689688668d802620d44f6296ec802eafb6b47f52
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-23 20:02:33 +0000

    added islower file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * @c: char
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int _islower(char c)
+{
+	int ch;
+	int r = 0;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (c == ch)
+		{
+			r = 1;
+		}
+	}
+	return(r);
+}
