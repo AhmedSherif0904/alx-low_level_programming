00aa02ffd09c26d620f6216b5fb8844aa11eadfb
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-24 14:42:21 +0000

    add added file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * add - entry point
+ *@a: number
+ *@b: number
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
