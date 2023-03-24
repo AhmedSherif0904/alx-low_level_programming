22d28e40e83a64082acc6162b97077377321949a
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-24 14:17:11 +0000

    added times table file

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * times_table - entry point
+ *
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void times_table(void)
+{
+	int i, j, mply;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			mply = i * j;
+
+			if (mply <= 9)
+			{
+				_putchar(mply + '0');
+			}
+			else
+			{
+				_putchar(mply / 10 + '0');
+				_putchar(mply % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
