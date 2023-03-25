a968aba56159631b1f38207f39510caf83a83df3
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-25 14:44:52 +0000

    added fib file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int main(void)
+{
+	long i, ro = 0, rn = 1, r = 0, sum;
+
+	for (i = 0; r < 4000000; i++)
+	{
+		r = ro + rn;
+		ro = rn;
+		rn = r;
+		if (r % 2 == 0)
+		{
+			sum = sum + r;
+			if (r < 4000000)
+			{
+				//printf(", ");
+			}
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
