09a96bd33ba35dd5ce594cc4e2c772588178e5e7
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-25 20:35:52 +0000

    added isupper file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+/**
+ * _isupper - entry point
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+int _isupper(char c)
+{
+	int ch, upper = 0;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if (c == ch)
+		{
+			upper = 1;
+		}
+	}
+	return (upper);
+}
