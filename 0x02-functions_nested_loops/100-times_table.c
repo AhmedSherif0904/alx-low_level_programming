2c817b584dfa83c25bfb5fc2305ee915eb63fa47
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-25 02:32:28 +0000

    added print to 98 file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * print_times_table - entry point
+ *@n: number
+ *
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void print_times_table(int n)
+{
+	int i, j, mply;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			mply = i * j;
+
+			if (mply <= 9)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(mply + '0');
+			}
+			else if (mply < 100)
+			{
+				_putchar(mply / 10 + '0');
+				_putchar(mply % 10 + '0');
+			}
+			else
+			{
+				_putchar(mply / 100 + '0');
+				_putchar((mply % 100) / 10 + '0');
+				_putchar((mply % 100) % 10 + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -14,72 +14,22 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (i = n; i < 98; i++)
 		{
-			if (n <= 9 && n >= 0)
-			{
-				_putchar(n + '0');
-			}
-			else if (n < 0 && n > -10)
-			{
-				_putchar('-');
-				_putchar(-n + '0');
-			}
-			else if (n <= -10)
-			{
-				_putchar('-');
-				_putchar(-n / 10 + '0');
-				_putchar(-n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
-			}
+			printf("%d, ", i);
 		}
 	}
-	else if (n == 98)
+	else
 	{
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
-	}
-	else if (n > 98)
-	{
-		for (; n >= 98; n--)
+		for (i = n; i > 98; i--)
 		{
-			if (n >= 100)
-			{
-			_putchar(n / 100 + '0');
-			_putchar((n % 10) / 10 + '0');
-			_putchar((n % 10) % 10 + '0');
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
-			}
+			printf("%d, ", i);
 		}
 	}
+	printf("98\n");
 }
+
