19bb43ca2fb63d103d0d39fa5749ac3dac09394c
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-25 03:00:15 +0000

    added print times table file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,39 +13,42 @@
 
 void print_times_table(int n)
 {
-	int i, j, mply;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n > 0)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			mply = i * j;
+		int i, j, mply;
 
-			if (mply <= 9)
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
 			{
-				if (j != 0)
+				mply = i * j;
+
+				if (mply <= 9)
 				{
+					if (j != 0)
+					{
+						_putchar(' ');
+					}
+					_putchar(mply + '0');
+				}
+				else if (mply < 100)
+				{
+					_putchar(mply / 10 + '0');
+					_putchar(mply % 10 + '0');
+				}
+				else
+				{
+					_putchar(mply / 100 + '0');
+					_putchar((mply % 100) / 10 + '0');
+					_putchar((mply % 100) % 10 + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
 					_putchar(' ');
 				}
-				_putchar(mply + '0');
 			}
-			else if (mply < 100)
-			{
-				_putchar(mply / 10 + '0');
-				_putchar(mply % 10 + '0');
-			}
-			else
-			{
-				_putchar(mply / 100 + '0');
-				_putchar((mply % 100) / 10 + '0');
-				_putchar((mply % 100) % 10 + '0');
-			}
-			if (j != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
