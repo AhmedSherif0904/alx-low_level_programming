e8323dc99394c21896e1c0de21c3c9c8da879f8c
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-30 17:38:36 +0000

    added print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - entry point
+ * @a: char
+ * @n: char
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		 printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
