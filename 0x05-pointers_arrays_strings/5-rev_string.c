e6ce95b5aecc3e84a8acba494cedec7936e40d32
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-03-30 15:52:43 +0000

    added rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - entry point
+ * @s: char
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	len = i - 1;
+
+	for (j = 0; j < len/2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i-1];
+		s[i--] = tmp;
+	}
+}
