329304281dc88a571db7a3900932e6c4ab063376
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-02 13:25:12 +0000

    added triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * @size : char
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
