5f89d3343a9640ef3c6d1823878e6c09f177fbd0
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-14 15:51:57 +0000

    added print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ *@s: char
+ * description : prints putchar
+ *
+ * Return: -1
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
