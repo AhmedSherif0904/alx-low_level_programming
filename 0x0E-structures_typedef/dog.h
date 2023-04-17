bc2dd084f59e0a8f7b918fa2dab481fed0b45443
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-17 22:09:21 +0000

    added dog struc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
-typedef struct dog
+/**
+ * struct dog - a dog's information
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the owner's name
+ *
+ * Description: struct containing information about a dog, including its name,
+ * age, and owner's name
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
