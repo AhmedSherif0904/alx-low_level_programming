beed8cdf80280509a182ec1af78106d5f6ed1218
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-18 00:03:23 +0000

    added new dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog_t struct
+ * @d: pointer to dog_t struct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
