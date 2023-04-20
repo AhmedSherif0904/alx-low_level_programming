3a9b6290f14cebe0c7f4b32706c690d8c3377bcd
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-20 00:32:31 +0000

    added 0 print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
 
-/* Function pointer to a function that takes an integer and returns nothing */
-typedef void (*int_func_ptr)(int);
-
-/* Function pointer to a function that takes a character pointer and returns nothing */
-typedef void (*char_func_ptr)(char *);
-
-/* Function prototype for print_name */
-void print_name(char *name, char_func_ptr f);
+void print_name(char *name, void (*f)(char *));
 
 #endif /* FUNCTION_POINTERS_H */
