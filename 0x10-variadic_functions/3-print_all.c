a1da7319f006c897c4d60c7f7213dd8878d8e70e
Ahmed Sherif <ahmedsherif0904@gmail.com>
2023-04-21 23:22:33 +0000

    added print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Description: c: char, i: integer, f: float, s: char * (if the string is NULL, print (nil) instead
+ * Any other char should be ignored.
+ * You are not allowed to use for, goto, ternary operator, else, do ... while
+ * You can use a maximum of 2 while loops, 2 if
+ * You can declare a maximum of 9 variables
+ * You are allowed to use printf
+ * Print a new line at the end of your function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+			default:
+				break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+					      format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
